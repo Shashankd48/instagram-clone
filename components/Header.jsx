ee4b855535a64626f1c7d93cef50f8ae2d13ea58
Shashank Dubey <jfriday464@gmail.com>
2021-10-17 20:44:20 +0530

    New fixes

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,11 +7,12 @@ import {
    MenuIcon,
    PaperAirplaneIcon,
 } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
    return (
       <header>
-         <div className="flex justify-between bg-white max-w-6xl ">
+         <div className="flex justify-between bg-white max-w-6xl mx-5 lg:mx-auto">
             <div className="relative hidden lg:inline-grid w-24  cursor-pointer">
                <Image
                   src="/instagram_logo.svg"
@@ -25,16 +26,34 @@ const Header = () => {
             </div>
 
             {/* Middle Section */}
-            <div className="mt-1 relative p-3 rounded-md ">
-               <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none ">
-                  <SearchIcon className="h-5 w-5 text-gray-500 " />
+            <div className="max-w-xs">
+               <div className="mt-1 relative p-3 rounded-md ">
+                  <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none ">
+                     <SearchIcon className="h-5 w-5 text-gray-500 " />
+                  </div>
+                  <input
+                     type="search"
+                     name="search"
+                     id="search"
+                     placeholder="Search"
+                     className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded"
+                  />
                </div>
-               <input
-                  type="search"
-                  name="search"
-                  id="search"
-                  placeholder="Search"
-                  className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded"
+            </div>
+
+            {/* Right Side */}
+            <div className="flex items-center justify-end space-x-4">
+               <HomeIcon className="navBtn" />
+               <MenuIcon className="h-6 md:hidden cursor-pointer" />
+               <PaperAirplaneIcon className="navBtn" />
+               <PlusCircleIcon className="navBtn" />
+               <UserGroupIcon className="navBtn" />
+               <HeartIcon className="navBtn" />
+
+               <img
+                  src="/user-avatar.jpeg"
+                  alt="User Avatar"
+                  className="h-10 rounded-full cursor-pointer"
                />
             </div>
          </div>
