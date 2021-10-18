b2b7855511bc31c78ac96bbd0711230ed83d67cf
Shashank Dubey <jfriday464@gmail.com>
2021-10-18 13:23:00 +0530

    Working on posts

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,5 +1,58 @@
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import {
+   HeartIcon as HeartIconOutlined,
+   BookmarkIcon,
+   ChatIcon,
+   DotsHorizontalIcon,
+   EmojiHappyIcon,
+   PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+
 const Post = ({ id, username, img, userImg, caption }) => {
-   return <div>I am post</div>;
+   return (
+      <div className="bg-white my-7 border rounded-sm">
+         {/*Header */}
+
+         <div className="flex items-center p-5">
+            <img
+               src={userImg}
+               alt="user image"
+               className="rounded-full h-12 w-12 object-contain border p-1 mr-3"
+            />
+            <p className="flex-1 font-medium">{username}</p>
+            <DotsHorizontalIcon className="h-5" />
+         </div>
+         {/*Img */}
+
+         <img src={img} alt="Post" className="object-cover w-full" />
+
+         {/*Buttons*/}
+
+         <div className="flex justify-between px-4 pt-4">
+            <div className="flex space-x-4">
+               <HeartIconOutlined className="post-btn" />
+               <ChatIcon className="post-btn" />
+               <PaperAirplaneIcon className="post-btn" />
+            </div>
+            <BookmarkIcon className="post-btn" />
+         </div>
+
+         {/* Caption */}
+         <p className="p-5 truncate">
+            <span className="font-medium">{username}</span>
+            <span className="ml-2">{caption}</span>
+         </p>
+
+         {/*Comments*/}
+
+         {/*input box*/}
+         <form className="flex items-center p-4">
+            <EmojiHappyIcon className="post-btn" />
+            <input type="text" className="" />
+            <button>Post</button>
+         </form>
+      </div>
+   );
 };
 
 export default Post;
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -5,33 +5,33 @@ const posts = [
       id: 123,
       username: "jfriday464",
       userImg:
-         "https://www.google.com/url?sa=i&url=http%3A%2F%2Fcristinawashere.com%2Fsummer-instagram-picture-ideas%2F&psig=AOvVaw14mRXq4NwEytYlCwrxtWEE&ust=1634616555353000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMiTjbeL0_MCFQAAAAAdAAAAABAD",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Finstagram-girl&psig=AOvVaw2pYgEwWydlWP24oZRVdNyG&ust=1634617146162000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNC_7ciN0_MCFQAAAAAdAAAAABAJ",
-      caption: "Hi",
+         "https://images.pexels.com/photos/9669217/pexels-photo-9669217.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      img: "https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      caption: "Beautiful view 😘",
    },
    {
       id: 124,
       username: "soniya123",
       userImg:
-         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F423268064961993684%2F&psig=AOvVaw0ZEU6OfQ9aA8HO-dl9lXgf&ust=1634616570204000&source=images&cd=vfe&ved=2ahUKEwjZ3a2xi9PzAhWLyDgGHfDnBaUQjRx6BAgAEAk",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.makebulog.com%2F2021%2F09%2F99-sad-girls-dp-for-whatsapp-facebook-instagram-profile-pics-2021.html&psig=AOvVaw2pYgEwWydlWP24oZRVdNyG&ust=1634617146162000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNC_7ciN0_MCFQAAAAAdAAAAABAP",
-      caption: "Hello",
+         "https://images.pexels.com/photos/3118694/pexels-photo-3118694.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      img: "https://images.pexels.com/photos/1174732/pexels-photo-1174732.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      caption: "Panama beach 🏖 chilling with friends🤗.",
    },
    {
       id: 125,
       username: "eva_roy99",
       userImg:
-         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fsofiasolisb.com%2Flifestyle%2Fsummer-captions-instagram-posts%2F&psig=AOvVaw06kxutnggh44MGeF44cbVy&ust=1634616572466000&source=images&cd=vfe&ved=2ahUKEwiE6reyi9PzAhXu5TgGHQdTDzsQjRx6BAgAEAk",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.techgrama.in%2Finstagram-dp-for-girls%2F&psig=AOvVaw2pYgEwWydlWP24oZRVdNyG&ust=1634617146162000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNC_7ciN0_MCFQAAAAAdAAAAABAc",
-      caption: "Hey",
+         "https://images.pexels.com/photos/8894629/pexels-photo-8894629.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      img: "https://images.pexels.com/photos/2215380/pexels-photo-2215380.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      caption: "I have love affair with mountains.",
    },
    {
       id: 126,
       username: "natasha14",
       userImg:
-         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F813955332628616048%2F&psig=AOvVaw1gnSnzMNNMeujwwfbVcokJ&ust=1634616702803000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPjN3vWL0_MCFQAAAAAdAAAAABAD",
-      img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freehindiwishes.com%2Fbest-dp-for-instagram-for-girl.html&psig=AOvVaw2pYgEwWydlWP24oZRVdNyG&ust=1634617146162000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNC_7ciN0_MCFQAAAAAdAAAAABAV",
-      caption: "Hey",
+         "https://images.pexels.com/photos/654690/pexels-photo-654690.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      img: "https://images.pexels.com/photos/848573/pexels-photo-848573.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      caption: "I love sunset and i lover myself.",
    },
 ];
 
@@ -45,7 +45,7 @@ const Posts = () => {
                username={post.username}
                img={post.img}
                userImg={post.userImg}
-               caption={post.username}
+               caption={post.caption}
             />
          ))}
       </div>
