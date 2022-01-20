aebab73e6e61caa76e1b9914c2c42c16d3dbf611
Shashank Dubey <jfriday464@gmail.com>
2022-01-20 18:59:46 +0530

    config var changes

---
+++ config.js
@@ -0,0 +1,16 @@
+const config = {
+   apiKey: process.env.APIKEY,
+   authDomain: process.env.AUTH_DOMAIN,
+   projectId: process.env.PROJECT_ID || "instagram-clone-98b68",
+   storageBucket: process.env.STORAGE_BUCKET,
+   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+   appId: process.env.APP_ID,
+   measurementId: process.env.MEASUREMENT_ID,
+   loadImages: process.env.LOADIMAGE,
+   secret: process.env.SECRET,
+   nextAuthURL: process.env.NEXTAUTH_URL,
+   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+   googleClientId: GOOGLE_CLIENT_ID,
+};
+
+export default config;
--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
+import config from "./config";
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,14 +9,24 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-   apiKey: "AIzaSyAFIJWKxEDfOFwO-i1-1_8S1lggKMOIlgU",
-   authDomain: "instagram-clone-98b68.firebaseapp.com",
-   projectId: "instagram-clone-98b68",
-   storageBucket: "instagram-clone-98b68.appspot.com",
-   messagingSenderId: "26831812486",
-   appId: "1:26831812486:web:a12c299f06d09ae561de07",
-   measurementId: "G-VESYJ294F4",
+   apiKey: config.apiKey,
+   authDomain: config.authDomain,
+   projectId: config.projectId,
+   storageBucket: config.storageBucket,
+   messagingSenderId: config.messagingSenderId,
+   appId: config.appId,
+   measurementId: config.measurementId,
 };
+
+// const firebaseConfig = {
+//    apiKey: process.env.APIKEY,
+//    authDomain: process.env.AUTH_DOMAIN,
+//    projectId: "instagram-clone-98b68",
+//    storageBucket: "instagram-clone-98b68.appspot.com",
+//    messagingSenderId: "26831812486",
+//    appId: "1:26831812486:web:a12c299f06d09ae561de07",
+//    measurementId: "G-VESYJ294F4",
+// };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
