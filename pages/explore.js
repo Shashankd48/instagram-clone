5053d256eed88dba1ef8bc7dbddfc086378f20d7
Shashank Dubey <jfriday464@gmail.com>
2022-01-21 10:13:47 +0530

    Explore page design fixes

--- pages/explore.js
+++ pages/explore.js
@@ -1,21 +1,29 @@
 import Page from "../components/Page";
 import { Fragment } from "react";
 import { getRandomPosts } from "../actions/PostAction";
+import Image from "next/image";
 
 const Explore = ({ posts }) => {
    return (
       <Fragment>
          <Page title="Instagram | Explore" />
-         <div className="max-w-4xl mx-auto mt-5">
-            <div class="flex flex-wrap -mx-1 overflow-hidden sm:-mx-2 xl:-mx-2">
+         <div className="max-w-4xl mx-auto mt-4">
+            <div className="flex flex-wrap -mx-4 overflow-hidden">
                {posts.map((post) => {
                   if (post.image)
                      return (
                         <div
-                           class="my-1 px-1 w-1/3 overflow-hidden sm:my-2 sm:px-2 sm:w-full md:w-1/2 xl:my-2 xl:px-2"
+                           className="my-1 px-1 w-1/3 overflow-hidden sm:my-1 sm:px-1 md:my-4 md:px-4"
                            key={post.id}
                         >
-                           <img src={post.image} alt="post" />
+                           <div className="w-full xl:h-[300px] md:h-[240px] h-[150px] sm:h-[180px] relative object-contain">
+                              <Image
+                                 src={post.image}
+                                 alt="post"
+                                 layout="fill"
+                                 objectFit="cover"
+                              />
+                           </div>
                         </div>
                      );
                })}
